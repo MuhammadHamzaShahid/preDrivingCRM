3c84314f8213ddca015b69179870fea10369c64c
Muhammad Hamza Shahid <busbuchoo@gmail.com>
2023-09-21 19:56:53 +0500

    booking logic hook updated

--- custom/modules/k_Bookings/bookingLogicHook.php
+++ custom/modules/k_Bookings/bookingLogicHook.php
@@ -5,7 +5,7 @@ class bookingLogicHookClass
 {
     public function bookingLogicHookMethod($bean, $event, $arguments)
     {
-            // Check if the test detail field has been update/edit
+        // Check if the test detail field has been update/edit
         if (empty($bean->fetched_row) || $bean->k_test_detail != $bean->fetched_row['k_test_detail']){
             if(!empty($bean->k_test_detail)){
                 // Extracting the data from the custom test detail field
@@ -22,10 +22,6 @@ class bookingLogicHookClass
                     $totalAmount = str_replace('£', '', trim($data[6]));
                     $lastDateToCancelF= date('Y-m-d',strtotime($lastDateToCancel)); 
                     $dateTimeF= date('Y-m-d H:i:s',strtotime($dateTime)); 
-
-                // Putting the trimmed values in their respective fields
-                    // $bean->name = $candidateName;
-                    // $bean->k_license_no = $drivingLicenseNumber;
                     $bean->k_test_center = $testCenter;
                     $bean->k_date_and_time = $dateTimeF;
                     $bean->k_driving_test_ref_no = $drivingReferenceNumber;
@@ -35,16 +31,14 @@ class bookingLogicHookClass
                 }
             }
         }
-    
         // Check if the swap field has been set to 'yes'
         if ($bean->k_swap === 'Yes') {
                 
             $currentTestDetail = $bean->k_test_detail;
-                $currentAccount = $bean->accounts_id;
-                $currentLicense = $bean->li_license_id;
-                $currentCustomerName = $bean->contacts_id;
-                
-                //Check if the test detail, account, license, and customer name fields are the same
+            $currentAccount = $bean->accounts_id;
+            $currentLicense = $bean->li_license_id;
+            $currentCustomerName = $bean->contacts_id;
+            //Check if the test detail, account, license, and customer name fields are the same
             if ($currentTestDetail != $bean->fetched_row['k_test_detail'] ||
                 $currentAccount != $bean->fetched_row['accounts_id'] ||
                 $currentLicense != $bean->fetched_row['li_license_id'] ||
@@ -52,26 +46,22 @@ class bookingLogicHookClass
            
                 // Check if the old test detail is empty
                 if (empty($bean->k_old_test_detail)) {
-                $bean->k_old_test_detail = $bean->k_test_detail;
+                    $bean->k_old_test_detail = $bean->k_test_detail;
                 } else {
-                $oldTestDetail = $bean->k_old_test_detail;
-                $oldTestDetail .= "\n" . $bean->k_test_detail;
-                $bean->k_old_test_detail = $oldTestDetail;
+                    $oldTestDetail = $bean->k_old_test_detail;
+                    $oldTestDetail .= "\n" . $bean->k_test_detail;
+                    $bean->k_old_test_detail = $oldTestDetail;
                 }
-
                 // Increment swap count
                 $currentSwapCount = (int)$bean->k_swap_count; // Convert to integer
                 $bean->k_swap_count = $currentSwapCount + 1;
-
                 // Increment swap fee by $20 for every swap
                 $currentSwapFee = $bean->k_swap_fee; // ignore '$' sign and convert to float while increment
                 $newSwapFee = (float)$currentSwapFee + 20;
                 $bean->k_swap_fee = $newSwapFee;
-
                 // Note swap date and save entries
                 $swapDate = date('d/m/Y');
                 $oldSwapDates = $bean->k_swap_date; // Retrieve existing swap dates
-
                  if (empty($oldSwapDates)) {
                 // No existing history, save the current value as the first entry
                 $bean->k_swap_date = "Swap " . $bean->k_swap_count . ": " . $swapDate;
@@ -79,7 +69,6 @@ class bookingLogicHookClass
                 // Append the new value to the existing history
                 $bean->k_swap_date = $oldSwapDates . "\n" . "Swap " . $bean->k_swap_count . ": " . $swapDate;
                 }
-                
                 // Create a new transaction record in the Transaction module
                 $transactionBean = BeanFactory::newBean('k_transactions');
                 $transactionBean->k_transaction_amount = '20';
@@ -87,69 +76,34 @@ class bookingLogicHookClass
                 $transactionBean->save();
             }
         }          
-                $bean->k_swap = 'No';
-                
-                if (empty($bean->accounts_id)) {
-                    $bean->discount = '';
+        $bean->k_swap = 'No';
+        
+        if (empty($bean->accounts_id)) {
+            $bean->discount = '';
+        }
+
+        $bean->total= (float)$bean->commission + (float)$bean->k_swap_fee + (float)$bean->test_fee -(float)$bean->discount;
+
+        if ($bean->contacts_id != '') {    
+            // Compute the new values based on the k_transaction_type
+            if($bean->k_transaction_type != $bean->fetched_row['k_transaction_type']){
+                $contactsBean = BeanFactory::getBean('Contacts', $bean->contacts_id);
+                if ($bean->k_transaction_type == 'Unpaid') {
+                    $unpaid = $bean->total;
+                    $contactsBean->k_unpaid_amount = $unpaid;
+                } elseif ($bean->k_transaction_type == 'Paid') {
+                    $paid = $bean->total;
+                    $contactsBean->k_paid_amount = $paid;
+                } elseif ($bean->k_transaction_type == 'Credit') {
+                    $credit = $bean->total;
+                    $contactsBean->k_credit_amount = $credit;
+                } elseif ($bean->k_transaction_type == 'Refund') {
+                    $refund = $bean->total;
+                    $contactsBean->k_refund_amount = $refund;
                 }
-
-                $bean->total= (float)$bean->commission + (float)$bean->k_swap_fee + (float)$bean->test_fee -(float)$bean->discount;
-
-                if ($bean->contacts_id != '') {
-                    // Check if the total field has changed
-                    // $previousTotal = $bean->fetched_row['total'];
-                    // if ($bean->total != $previousTotal) {
-                        // Retrieve the Contacts bean
-                        $contactsBean = BeanFactory::getBean('Contacts', $bean->contacts_id);
-                
-                        // Compute the new values based on the k_transaction_type
-                        if($bean->k_transaction_type != $bean->fetched_row['k_transaction_type']){
-                        if ($bean->k_transaction_type == 'Unpaid') {
-                            $unpaid = $bean->total;
-                            $contactsBean->k_unpaid_amount = $unpaid;
-                        } elseif ($bean->k_transaction_type == 'Paid') {
-                            $paid = $bean->total;
-                            $contactsBean->k_paid_amount = $paid;
-                        } elseif ($bean->k_transaction_type == 'Credit') {
-                            $credit = $bean->total;
-                            $contactsBean->k_credit_amount = $credit;
-                        } elseif ($bean->k_transaction_type == 'Refund') {
-                            $refund = $bean->total;
-                            $contactsBean->k_refund_amount = $refund;
-                        }
-                        
-                        // $unpaidAmount = $contactsBean->k_unpaid_amount;
-                        // $paidAmount = $contactsBean->k_paid_amount;
-                        // $creditAmount = $contactsBean->k_credit_amount;
-                        // $refundAmount = $contactsBean->k_refund_amount;
-                        // $contactsBean->amount = (int)$unpaidAmount - (int)$paidAmount - (int)$creditAmount;
-                        // // Save the changes to the Contacts bean
-                        // $contactsBean->save();
-                    }
-                }
-
-                
-            // if($bean->contacts_id!=''){
-            //     // Query to get the current count of bookings transactions
-            //     $query = "SELECT SUM(total) AS sum FROM k_bookings WHERE contacts_id = '{$bean->contacts_id}' AND id != '{$bean->id}' AND (k_status = 'Direct' OR k_status = 'Confirmed') AND deleted = '0'";
-               
-            //     $result = $GLOBALS['db']->query($query);
-            //     $row = $GLOBALS['db']->fetchByAssoc($result);
-            //     $sum = $row['sum'] + $bean->total;
-            //     $contactsBean = BeanFactory::getBean('Contacts',$bean->contacts_id);
-            //      // Update the amount field in the Contacts module
-            //     $contactsBean = BeanFactory::getBean('Contacts', $bean->contacts_id);
-            //     die($sum);
-            //     if ($bean->k_status == 'Cancelled') {
-            //     // Deduct the total amount when the status is "Cancelled"
-            //     $contactsBean->amount = $contactsBean->amount - $bean->total;
-            //     } else {
-            //     // Add the total amount for other statuses
-            //     $contactsBean->amount = $sum;
-            //     }
-
-            //     $contactsBean->save();
-            // } 
+                $contactsBean->save();
+            }
+        }
     }
 }
 
