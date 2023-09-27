7b71659c0da5e65f6ae587ee4d3ce1ceeed44b90
usman-thearrow <muhammadusmanzahid86@gmail.com>
2023-09-27 13:32:41 +0500

    add available value in booking status dom list

--- custom/Extension/application/Ext/Language/en_us.status_list_dom.php
+++ custom/Extension/application/Ext/Language/en_us.status_list_dom.php
@@ -1,6 +1,7 @@
 <?php
    $app_list_strings['k_status_list'] = array(
       '' => '',
+      'Available' => 'Available',
       'Direct' => 'Direct',
       'Confirmed' => 'Confirmed',
       'On Hold' => 'On Hold',
