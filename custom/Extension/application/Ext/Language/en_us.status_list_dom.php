b17ac276df12e4d4176eba8b59ea7685de640e5b
usman-thearrow <muhammadusmanzahid86@gmail.com>
2023-09-27 15:38:25 +0500

    revert the changes in Booking status field

--- custom/Extension/application/Ext/Language/en_us.status_list_dom.php
+++ custom/Extension/application/Ext/Language/en_us.status_list_dom.php
@@ -1,7 +1,6 @@
 <?php
    $app_list_strings['k_status_list'] = array(
       '' => '',
-      'Available' => 'Available',
       'Direct' => 'Direct',
       'Confirmed' => 'Confirmed',
       'On Hold' => 'On Hold',
