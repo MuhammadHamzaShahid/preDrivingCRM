97760857649ce03975fc14376531ca1dcd5cf9b6
usman-thearrow <137076682+usman-thearrow@users.noreply.github.com>
2023-10-03 09:58:18 +0500

    create WhatsApp chat button at booking detail view

--- custom/modules/k_Bookings/views/view.detail.php
+++ custom/modules/k_Bookings/views/view.detail.php
@@ -27,6 +27,18 @@ class k_BookingsViewDetail extends ViewDetail {
           alert('Payment link copied to clipboard!');
       }
       </script>";
+
+      $phone_mobile = $this->bean->k_phone_no;
+      $phone_mobileExp = explode('+', $phone_mobile);
+      $phone_mobile = $phone_mobileExp[1];
+      echo "<button style='padding: 10px 20px; background-color: #25D366; color: #fff; border: none; border-radius: 4px; font-size: 16px; cursor: pointer;' onclick='redirectToWatiChat(\"$phone_mobile\")'>WhatsApp Chat</button>";
+      // wati chats script
+      echo "<script>
+      function redirectToWatiChat(phoneNumber) {
+        var watiUrl = 'https://live-115159.wati.io/teamInbox?filter={%22filterType%22:0}&search={%22searchString%22:' + phoneNumber + '}';        // Open the chatbox in a new window or tab
+        window.open(watiUrl, '_blank');
+    }
+      </script>";
   parent::display();  
     }
 }
