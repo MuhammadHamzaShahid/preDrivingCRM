eee65ea73b970b46bae8fae289d373d149fffce4
Muhammad Hamza Shahid <busbuchoo@gmail.com>
2023-10-16 15:30:28 +0500

    phone no removed from mandatory

--- custom/Extension/modules/k_Bookings/Ext/Vardefs/sugarfield_k_phone_no.php
+++ custom/Extension/modules/k_Bookings/Ext/Vardefs/sugarfield_k_phone_no.php
@@ -5,6 +5,6 @@ $dictionary['k_Bookings']['fields']['k_phone_no'] = array(
     'vname' => 'LBL_K_PHONE_NO',
     'type' => 'varchar',
     'len' => '255',
-    'required' => true,
+    'required' => false,
     'audited' => false,
 );
